f97a3cc3f3a15d1b2a7f47e9fd6c812e26bc6a36
m sochabas <65498666+abas32-izwell@users.noreply.github.com>
2022-10-30 00:28:53 +0700

    Create 468×60.js

---
+++ 468×60.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '3bafead10164bb0279b8172b5e94da67',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/3bafead10164bb0279b8172b5e94da67/invoke.js"></scr' + 'ipt>');
