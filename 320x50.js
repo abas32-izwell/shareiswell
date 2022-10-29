56856988aca918018ee0676191dd588fccff694b
m sochabas <65498666+abas32-izwell@users.noreply.github.com>
2022-10-30 04:36:37 +0700

    Create 320x50.js

---
+++ 320x50.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '6923b216cb29bfd7190de89fa24b1f5b',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/6923b216cb29bfd7190de89fa24b1f5b/invoke.js"></scr' + 'ipt>');
