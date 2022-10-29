710330d55617aa4dd0d6c9e1e97bd40e5d8d18bb
m sochabas <65498666+abas32-izwell@users.noreply.github.com>
2022-10-30 00:26:04 +0700

    Create 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'ad324b7fe7fa91544c6c782fd6cb76bd',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/ad324b7fe7fa91544c6c782fd6cb76bd/invoke.js"></scr' + 'ipt>');
