fa0cc6abd1b2aa836599ec9449f00658eb68598a
m sochabas <65498666+abas32-izwell@users.noreply.github.com>
2022-10-30 00:31:03 +0700

    Create directpopup.js

---
+++ directpopup.js
@@ -0,0 +1,12 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://capricedes.com/rz40z9es2?key=738d841ec2d3a7e98674d37c35f3e28c', '_blank');
+	});
