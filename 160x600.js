c69b0904fee938cb482e420a40ab20bf4aaa019f
m sochabas <65498666+abas32-izwell@users.noreply.github.com>
2022-10-30 04:46:57 +0700

    Create 160x600.js

---
+++ 160x600.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '393e577260e117ddc8e81dc8825760ee',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/393e577260e117ddc8e81dc8825760ee/invoke.js"></scr' + 'ipt>');
